ba7c9777ff07266b1d8895488419c67fa8299037
Catherene Joshi <catherene.joshi@gmail.com>
2022-01-30 19:54:00 -0800

    frontend

---
+++ frontend/src/app/components/bg1/bg1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bg1',
+  templateUrl: './bg1.component.html',
+  styleUrls: ['./bg1.component.css']
+})
+export class Bg1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
